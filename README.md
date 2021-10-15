9662511448882fe1bd09de9c97a26ffb92413638
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2021-10-15 14:32:06 +0300

    Improved setup.sh and model now v1.12.3 Нашел компромисс между использованием старой, но хорошо работающей версией автопилота и текущей версией: использую тег v1.12.3. На ее основе и сделаны данные модели.
    Улучшил скрипт настройки, теперь так куча разных проверок
    
    Нормально оформил README

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # gardener-drone
 Drone model (modified Iris) for Gazebo simulator.
+Модифицированный Iris c ROS камерой для Gazebo simulator.
 
-To use the model, run `setup.sh PATH_TO_PX4`. This script will make all the necessary changes to PX4-Autopilot.
+Так как PX4-Autopilot быстро развивается, за основу взята стабильная версия 1.12.3.  
+Клонировать:  
+```bash
+git clone --recurse-submodules --branch v1.12.3 https://github.com/PX4/PX4-Autopilot.git
+```
+
+Добавьте модель, запустив `setup.sh PATH_TO_PX4`. Этот скрипт внесет все необходимые изменения в PX4-Autopilot.
+
+Для запуска симуляции используйте:
+```bash
+make px4_sitl gazebo_gardener_iris
+```
+или (с камерой):
+```bash
+make px4_sitl gazebo_gardener_drone
+```
+
+## Известные проблемы
+1. gardener_drone взлетает без камеры. Она остается на земле.
--- setup.sh
+++ setup.sh
@@ -1,34 +1,94 @@
 #!/bin/bash
+# https://discuss.px4.io/t/create-custom-model-for-sitl/6700/17
 # https://github.com/willinum/SPEC21/wiki/custom-model-for-PX4-sitl-GAZEBO-simulation
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+NOCOLOR='\033[0m'
 
 if [ -z $1 ] || [ $1 == "--help" ]; then
     echo "Usage: setup.sh PATH_TO_PX4"
-    echo "       For example:"
-    echo "       setup.sh /home/user/PX4-Autopilot"
-elif ! [ -d $1 ]; then
-    echo "No directory"
+    echo "  For example:"
+    echo "  setup.sh /home/user/PX4-Autopilot"
+elif [ ! -d $1 ]; then
+    echo -en "${RED}[ERR]: ${NOCOLOR}"
+    echo "Directory $1 not found"
 else
     SCRIPT=$(realpath $0)
     SCRIPTPATH=$(dirname $SCRIPT)
-    cp -r $SCRIPTPATH/models/* $1/Tools/sitl_gazebo/models
-    cp $SCRIPTPATH/airframes/* $1/ROMFS/px4fmu_common/init.d-posix/airframes
-    cp $SCRIPTPATH/airframes/* $1/build/px4_sitl_default/etc/init.d-posix/airframes
-    # Uncomment for RTPS
-    #cp $SCRIPTPATH/airframes/* $1build/px4_sitl_rtps/etc/init.d-posix/airframes
 
-    lineNum=$(grep -n "set(models" $1/platforms/posix/cmake/sitl_target.cmake | head -n 1 | cut -d: -f1)
-    lineNum=$((lineNum+1))
-    gardIrisOK=$(grep -n "gardener_iris" $1/platforms/posix/cmake/sitl_target.cmake | head -n 1 | cut -d: -f1)
-    if [ -z $gardIrisOK ]; then
-        sed -i "${lineNum}i \\\tgardener_iris" $1/platforms/posix/cmake/sitl_target.cmake
+    #1 Create a model under Tools/sitl_gazebo/models
+    MODELS=$1/Tools/sitl_gazebo/models
+    if [ -d $MODELS ]; then
+        cp -r $SCRIPTPATH/models/* $MODELS
     else
-        echo "gardener_iris already contains in sitl_target.cmake"
+        echo -en "${RED}[ERR]: ${NOCOLOR}"
+        echo "Directory $MODELS not found"
     fi
-    gardDroneOK=$(grep -n "gardener_drone" $1/platforms/posix/cmake/sitl_target.cmake | head -n 1 | cut -d: -f1)
-    if [ -z $gardIrisOK ]; then
-        sed -i "${lineNum}i \\\tgardener_drone" $1/platforms/posix/cmake/sitl_target.cmake
+
+    #2 Create a world file in Tools/sitl_gazebo/worlds
+    WORLDS=$1/Tools/sitl_gazebo/worlds
+    if [ -d $WORLDS ]; then
+        cp -r $SCRIPTPATH/worlds/* $WORLDS
     else
-        echo "gardener_drone already contains in sitl_target.cmake"
+        echo -en "${RED}[ERR]: ${NOCOLOR}"
+        echo "Directory $WORLDS not found"
+    fi
+
+    #3 Create an airframe file under ROMFS/px4fmu_common/init.d-posix/airframes
+    AIRFRAMES=$1/ROMFS/px4fmu_common/init.d-posix/airframes
+    if [ -d $AIRFRAMES ]; then
+        cp $SCRIPTPATH/airframes/* $AIRFRAMES
+    else
+        echo -en "${RED}[ERR]: ${NOCOLOR}"
+        echo "Directory $AIRFRAMES not found"
+    fi
+
+    #4 Add the airframe name to the file platforms/posix/cmake/sitl_target.cmake
+    FILE_SITL_TARGET=$1/platforms/posix/cmake/sitl_target.cmake
+    if [ -f $FILE_SITL_TARGET ]; then
+        lineNum=$(grep -n "set(models" $FILE_SITL_TARGET | head -n 1 | cut -d: -f1)
+        lineNum=$((lineNum+1))
+        gardIrisOK=$(grep -n "gardener_iris" $FILE_SITL_TARGET | head -n 1 | cut -d: -f1)
+        if [ -z $gardIrisOK ]; then
+            sed -i "${lineNum}i \\\tgardener_iris" $FILE_SITL_TARGET
+        else
+            echo -en "${YELLOW}[WARNING]: ${NOCOLOR}"
+            echo "gardener_iris already contains in sitl_target.cmake"
+        fi
+        gardDroneOK=$(grep -n "gardener_drone" $FILE_SITL_TARGET | head -n 1 | cut -d: -f1)
+        if [ -z $gardIrisOK ]; then
+            sed -i "${lineNum}i \\\tgardener_drone" $FILE_SITL_TARGET
+        else
+            echo -en "${YELLOW}[WARNING]: ${NOCOLOR}"
+            echo "gardener_drone already contains in sitl_target.cmake"
+        fi
+    else
+        echo -en "${RED}[ERR]: ${NOCOLOR}"
+        echo "File $FILE_SITL_TARGET not found"
+    fi
+
+    #5 Add the airframe name to the file ROMFS/px4fmu_common/init.d-posix/airframes/CMakeLists.txt
+    FILE_CMAKELISTS=$1/ROMFS/px4fmu_common/init.d-posix/airframes/CMakeLists.txt
+    if [ -f $FILE_CMAKELISTS ]; then
+        lineNum=$(grep -n "px4_add_romfs_files(" $FILE_CMAKELISTS | head -n 1 | cut -d: -f1)
+        lineNum=$((lineNum+1))
+        gardIrisOK=$(grep -n "131301_gardener_iris" $FILE_CMAKELISTS | head -n 1 | cut -d: -f1)
+        if [ -z $gardIrisOK ]; then
+            sed -i "${lineNum}i \\\t131301_gardener_iris" $FILE_CMAKELISTS
+        else
+            echo -en "${YELLOW}[WARNING]: ${NOCOLOR}"
+            echo "gardener_iris already contains in CMakeLists.txt"
+        fi
+        gardDroneOK=$(grep -n "131302_gardener_drone" $FILE_CMAKELISTS | head -n 1 | cut -d: -f1)
+        if [ -z $gardIrisOK ]; then
+            sed -i "${lineNum}i \\\t131302_gardener_drone" $FILE_CMAKELISTS
+        else
+            echo -en "${YELLOW}[WARNING]: ${NOCOLOR}"
+            echo "gardener_drone already contains in CMakeLists.txt"
+        fi
+    else
+        echo -en "${RED}[ERR]: ${NOCOLOR}"
+        echo "File $FILE_CMAKELISTS not found"
     fi
 
     echo "Setup completed"
